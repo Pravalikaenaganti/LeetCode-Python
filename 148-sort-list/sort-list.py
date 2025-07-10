1b48f0f8e39a44170485e0f65005dc547b0c74ba
Pravalika <pravalikaenaganti222@gmail.com>
2025-07-10 10:48:24 +0530

    Time: 212 ms (19.93%) | Memory: 32.7 MB (87.13%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,19 +1,39 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def findMiddle(head):
+    slow = head
+    fast = head
+    prev = None
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+    if prev:
+        prev.next = None  
+    return slow
+
+def merge(left, right):
+    dummy = ListNode()
+    tail = dummy
+    while left and right:
+        if left.val < right.val:
+            tail.next = left
+            left = left.next
+        else:
+            tail.next = right
+            right = right.next
+        tail = tail.next
+    tail.next = left or right
+    return dummy.next
+
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp=head
-        l = []
-        while(temp!=None):
-            l.append(temp.val)
-            temp=temp.next
-        l.sort()
-        temp=head
-        for val in l:
-            temp.val=val
-            temp=temp.next
-        return head
-        
+    def sortList(self, head): 
+        if not head or not head.next:
+            return head
+
+        mid = findMiddle(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        return merge(left, right)
